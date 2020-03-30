1955d3fef9531a9818dd85acc1e7ebc7b50aad5b
raphaelbenevides18 <raphael.rbinformatica@gmail.com>
2020-03-30 16:03:30 -0300

    Projeto criado

---
+++ src/main/java/br/com/rlb/projetoSpringIonic/ProjetoSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package br.com.rlb.projetoSpringIonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rlb/projetoSpringIonic/ProjetoSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rlb.projetoSpringIonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
