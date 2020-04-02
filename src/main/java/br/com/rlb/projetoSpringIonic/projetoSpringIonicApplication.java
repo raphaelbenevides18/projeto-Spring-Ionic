107fd918248a08e06273c82b853c7281b7c911b2
raphaelbenevides18 <raphael.rbinformatica@gmail.com>
2020-04-02 01:16:50 -0300

    classe produto, associacoes e tratamento de excecoes

--- src/main/java/br/com/rlb/projetoSpringIonic/projetoSpringIonicApplication.java
+++ src/main/java/br/com/rlb/projetoSpringIonic/projetoSpringIonicApplication.java
@@ -1,13 +1,40 @@
 package br.com.rlb.projetoSpringIonic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rlb.projetoSpringIonic.entity.Categoria;
+import br.com.rlb.projetoSpringIonic.entity.Produto;
+import br.com.rlb.projetoSpringIonic.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class projetoSpringIonicApplication {
+public class projetoSpringIonicApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(projetoSpringIonicApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		//Produto p1 = new Produto(null,"computador",2000.);
+		//Produto p2 = new Produto(null,"imoressora",800.);
+		//Produto p3 = new Produto(null,"mouser",80.);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+	}
+	
+	
 
 }
--- src/main/java/br/com/rlb/projetoSpringIonic/service/CategoriaService.java
+++ src/main/java/br/com/rlb/projetoSpringIonic/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.rlb.projetoSpringIonic.entity.Categoria;
 import br.com.rlb.projetoSpringIonic.repository.CategoriaRepository;
+import br.com.rlb.projetoSpringIonic.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = dao.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 
 }
