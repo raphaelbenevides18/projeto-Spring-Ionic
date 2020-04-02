cb9aaacc4481fbfdae61ae39936d9708657b2e22
raphaelbenevides18 <raphael.rbinformatica@gmail.com>
2020-04-02 17:51:37 -0300

    classe cliente, tipocliente,telefones e enderecos

---
+++ src/main/java/br/com/rlb/projetoSpringIonic/repository/CidadeRepository.java
@@ -0,0 +1,10 @@
+package br.com.rlb.projetoSpringIonic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rlb.projetoSpringIonic.entity.Cidade;
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/br/com/rlb/projetoSpringIonic/repository/ClienteRepository.java
@@ -0,0 +1,5 @@
+package br.com.rlb.projetoSpringIonic.repository;
+
+public interface ClienteRepository {
+
+}
